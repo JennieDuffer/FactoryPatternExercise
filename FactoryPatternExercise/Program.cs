b2e41770fc822da164935b001e59930e90f953f0
Jennie Duffer <jenniej297@gmail.com>
2022-08-25 10:56:28 -0500

    initial commit

---
+++ FactoryPatternExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿using FactoryPatternExercise;
+
+Console.WriteLine("What vehicle do you want to drive?");
+var response = Console.ReadLine();
+
+IVehicle vehicle = VehicleFactory.GetVehicle(response);
+
+vehicle.Drive();
---
+++ FactoryPatternExercise/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string numberOfTires)
+        {
+            switch (numberOfTires)
+            {
+                case "car":
+                    return new Car();
+                case "truck":
+                    return new Truck();
+                default:
+                    return new Truck();
+
+            }
+        }
+
+    }
+}
